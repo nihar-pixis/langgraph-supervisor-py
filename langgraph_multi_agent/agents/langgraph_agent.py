3b128f2e932164436e38f6016f6b265f0df062f5
vbarda <vadym@langchain.dev>
2025-02-05 16:32:17 -0500

    nit

--- langgraph_multi_agent/agents/langgraph_agent.py
+++ langgraph_multi_agent/agents/langgraph_agent.py
@@ -101,12 +101,12 @@ class LangGraphAgent(PregelProtocol):
 
     name: str
     """Agent name. Will be used as a node name in the multi-agent workflow."""
+    agent: PregelProtocol
+    """Underlying agent implementation."""
     is_entrypoint: bool
     """Whether the agent serves as an entrypoint in the multi-agent system"""
     always_handoff_to: list[str] | None
     """List of agent names (nodes) to always add edges to"""
-    agent: PregelProtocol
-    """Underlying agent implementation."""
     agent_input_strategy: AgentInputStrategy = "full_history"
     """How is information passed to the agent"""
     agent_output_strategy: AgentOutputStrategy = "full_history"
